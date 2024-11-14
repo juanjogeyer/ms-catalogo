1e7cacd82ad231b379e06a717efd5d06d8f5d68f
Juan Geyer <juanjogeyer@gmail.com>
2024-11-14 18:00:08 -0300

    Reapply "Add cache"
    This reverts commit 347cd0e1bef3498ee539bb7a15faf6f689e56769.

--- app/services/producto_service.py
+++ app/services/producto_service.py
@@ -1,5 +1,6 @@
 from app.models import Producto
 from app.producto_repository import ProductoRepository
+from app import cache
 from flask import abort
 
 repository = ProductoRepository()
@@ -7,7 +8,18 @@ repository = ProductoRepository()
 class ProductoService:
 
     def find(self, id: int) -> Producto:
-        product = repository.find(id)
-        if product is None:
-            abort(404, description="Product not found")
-        return product
+        result = None
+        if id is not None:
+            result = cache.get(f"producto_{id}")
+
+            if result is not None:
+                print(f"Producto {id} obtenido del caché")  # Agregamos un mensaje de depuración
+            else:
+                print(f"Producto {id} no está en el caché. Consultando el repositorio...")
+                result = repository.find(id)
+
+                if result is None:
+                    abort(404, description="Product not found")
+                cache.set(f"producto_{id}", result, timeout=50)
+                
+        return result
