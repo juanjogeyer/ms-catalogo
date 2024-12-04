97e0c0b1a8ca21abd21b19453a9526557b633d0b
Juan Geyer <juanjogeyer@gmail.com>
2024-12-04 12:23:31 -0300

    Add lock

--- app/services/producto_service.py
+++ app/services/producto_service.py
@@ -2,24 +2,29 @@ from app.models import Producto
 from app.producto_repository import ProductoRepository
 from app import cache
 from flask import abort
+from threading import Lock
 
 repository = ProductoRepository()
 
 class ProductoService:
 
+    def __init__(self):
+        self.lock = Lock()
+
     def find(self, id: int) -> Producto:
-        result = None
-        if id is not None:
-            result = cache.get(f"producto_{id}")
+        with self.lock:
+            result = None
+            if id is not None:
+                result = cache.get(f"producto_{id}")
 
-            if result is not None:
-                print(f"Producto {id} obtenido del caché")  # Agregamos un mensaje de depuración
-            else:
-                print(f"Producto {id} no está en el caché. Consultando el repositorio...")
-                result = repository.find(id)
+                if result is not None:
+                    print(f"Producto {id} obtenido del caché")  # Agregamos un mensaje de depuración
+                else:
+                    print(f"Producto {id} no está en el caché. Consultando el repositorio...")
+                    result = repository.find(id)
 
-                if result is None:
-                    abort(404, description="Product not found")
-                cache.set(f"producto_{id}", result, timeout=50)
-                
-        return result
+                    if result is None:
+                        abort(404, description="Product not found")
+                    cache.set(f"producto_{id}", result, timeout=50)
+
+            return result
